f823a62306d04ed6357e4cb8dfd2263a09cd99e4
Aiden <km480@duke.edu>
2020-11-20 17:33:36 -0800

    update export calander

--- Xaml-Controls-Gallery-master/XamlControlsGallery/ControlPages/ScrollViewerPage.xaml.cs
+++ Xaml-Controls-Gallery-master/XamlControlsGallery/ControlPages/ScrollViewerPage.xaml.cs
@@ -8,6 +8,9 @@
 //
 //*********************************************************
 using System;
+using System.Net;
+using Windows.Storage;
+//using Microsoft.UI.Xaml.Controls;
 using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -35,9 +38,23 @@ namespace AppUIBasics.ControlPages
 
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private async void Button_Click_1(object sender, RoutedEventArgs e)
         {
+            System.Threading.Thread.Sleep(100);
+            var message = new MessageDialog("Calander export successfully!");
+            await message.ShowAsync();
+            StorageFolder localfolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+            using (WebClient wc = new WebClient())
+            {
+                //wc.DownloadProgressChanged += wc_DownloadProgressChanged;
+                wc.DownloadFile(new Uri("https://www.phpclasses.org/browse/download/1/file/63438/name/example.ics"), localfolder.Path+"/example.ics");
+            }
+            
+        }
 
+        private void progress_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            
         }
     }
 }
